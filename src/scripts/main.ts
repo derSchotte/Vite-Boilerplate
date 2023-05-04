ee115344848b68a20102e5534c1d5739d0304eaa
Frank McDonald <kontakt@frank-mcdonald.de>
2023-05-04 07:59:00 +0200

    Push

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,6 @@
 import '.././styles/style.scss';
+import * as bootstrap from 'bootstrap';
+
 // import typescriptLogo from '.././typescript.svg';
 // import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
