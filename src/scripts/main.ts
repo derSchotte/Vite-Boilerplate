f7b19ddc94d0a2cf11edada9ff50c2841f504955
Frank McDonald <kontakt@frank-mcdonald.de>
2023-05-04 12:52:04 +0200

    reconfigure scss and css for bootstrap and icons

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,26 +1,23 @@
 import '.././styles/style.scss';
 import * as bootstrap from 'bootstrap';
 
-// import typescriptLogo from '.././typescript.svg';
-// import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1 class="fw-bold pb-3">Vite Boilerplate</h1>
+            <p>
+                Diese Boilerplate ist für die Erstellung von<br />
+                <span class="fst-italic fw-bold"
+                    >HTML5, SCSS, JAVASCRIPT/TYPESCRIPT und SQL-Anbindung</span
+                ><br />
+                in Verbindung mit
+                <span class="fst-italic fw-bold"> Bootstrap</span> sowie
+                <span class="fst-italic fw-bold">Bootstrap-icons</span> gedacht.
+            </p>
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+            <p>
+                Dafür sind schon alle nötigen Erweiterungen und Dateien
+                vorkonfiguriert. Um eine Life-Preview wärend des Programmieren
+                der Seite zu erhalten.<br/> Starten sie im Terminal den Befehl:
+                <code class="fw-bold" id="code_highlight">npm run dev</code>.
+            </p>
+            <p>Viel Spass beim Programmieren...</p>
+`;
